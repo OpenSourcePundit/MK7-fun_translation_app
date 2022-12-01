531e87b27c6c5b59c5495de017f6ce067f4c18a6
OpenSourcePundit <shashank00098@gmail.com>
2022-12-02 03:37:35 +0530

    final rendering done.

--- app.js
+++ app.js
@@ -8,19 +8,17 @@ function getTranslationURL(text){
     return serverURL+"?text="+text;
 }
 
-function errorHandler(error){ //errorhandling using catch , this is a callback function
+function errorHandler(error){ //errorhandling 
     console.log("Error Occured"+error);
     alert("Error Occured, Please try after sometime")
 }
 
 function clickHandler() {    
     var inputText = txtInput.value; //extracting inputText
-    //fetch using translation URL, calling server for processing
-   fetch(getTranslationURL(inputText)) 
+    fetch(getTranslationURL(inputText)) 
    .then(read => read.json())
    .then(reads => {var translatedText=reads.contents.translated;
-    outputDiv.innerText=translatedText;}) //output as innertext
+    outputDiv.innerText=translatedText;})
     .catch(errorHandler)
    }
-
    btnTranslate.addEventListener("click",clickHandler);
